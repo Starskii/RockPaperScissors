1e9b0390246636e44a45d2c12ba4b4f20635240e
Starskii <jakeylakie@yahoo.com>
2024-08-30 21:08:47 -0700

    added full game functionality with the UI integrated.

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ let winningCombinations = {
     "scissors": "paper"
 };
 
+let humanScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
     return choices[computerChoice];
@@ -33,38 +36,56 @@ const getUserChoice = () => {
 
 const playRound = (humanSelection, computerSelection) => {
     returnValue = 0;
+    let messageString = "";
     if(winningCombinations[humanSelection] === computerSelection){
-        console.log(`You win! ${humanSelection} beats ${computerSelection}`);
+        messageString = `You win! ${humanSelection} beats ${computerSelection}`;
         returnValue = 1;
     }
     else if(winningCombinations[computerSelection] === humanSelection){
-        console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+        messageString = `You lose! ${computerSelection} beats ${humanSelection}`;
         returnValue = 2;
     }
     else{
-        console.log(`It's a tie. You both chose ${humanSelection}!`);
+        messageString = `It's a tie. You both chose ${humanSelection}!`;
     }
+    messageLabel.innerHTML = messageString;
     return returnValue;
 };
 
-const playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
-    let rounds = 5;
-
-    for(let round = 0; round < rounds; round++){
-        const roundOutcome = playRound(getUserChoice(), getComputerChoice());
-        switch(roundOutcome){
-            case 1: 
-                humanScore++;
-                break;
-            case 2:
-                computerScore++;
-                break;
-        }
+const playGame = (e) => {
+    const userChoice = e.target.innerHTML.toLowerCase();
+    const roundOutcome = playRound(userChoice, getComputerChoice());
+    switch(roundOutcome){
+        case 1: 
+            humanScore++;
+            break;
+        case 2:
+            computerScore++;
+            break;
     }
 
-    console.log(`The final score was \nHuman: ${humanScore} \nComputer: ${computerScore}`);
+    if(humanScore === 5){
+        alert("You won!");
+        humanScore = 0;
+        computerScore = 0;
+        messageLabel.innerHTML = "Press a button to play!";
+    }
+    if(computerScore === 5){
+        alert("Computer won!");
+        humanScore = 0;
+        computerScore = 0;
+        messageLabel.innerHTML = "Press a button to play!";
+    }
+
+    humanScoreLabel.innerHTML =  `Human Score: ${humanScore}`;
+    computerScoreLabel.innerHTML = `Computer Score: ${computerScore}`;
 }
 
-//playGame(); 
+const messageLabel = document.querySelector(".message-label");
+const humanScoreLabel = document.querySelector(".human-score");
+const computerScoreLabel = document.querySelector(".computer-score");
+
+const buttons = document.querySelectorAll(".user-controls button");
+for(btn of buttons){
+    btn.addEventListener("click", playGame)
+}
